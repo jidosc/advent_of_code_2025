759cc949357d3a9592019dff5cfcac56ce29e1b0
JidOsc <84289227+JidOsc@users.noreply.github.com>
2025-12-01 12:24:44 +0100

    Day 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,30 +1,51 @@
 inputs: list = open("input.txt", "r").read().splitlines()
-zero_times: int = 0
-sum: int = 50
 
-for inp in inputs:
-    mag = int(inp[1:])
-    mag = -mag if inp[0] == "L" else mag
 
-    hundreds = abs(mag) // 100
-    if hundreds != 0:
-        zero_times += hundreds
-        if mag <= -100:
-            mag += hundreds * 100
-        elif mag >= 100:
-            mag -= hundreds * 100
+def part1():
+    zero_times: int = 0
+    sum: int = 50
 
-    old_sum = sum
-    sum = (sum + mag)
-    if (mag < 0):
-        if (old_sum > 0 and sum < 0):
-            zero_times += 1
-    elif (mag > 0):
-        if (old_sum < 100 and sum > 100):
+    for inp in inputs:
+        mag = int(inp[1:])
+        mag = -mag if inp[0] == "L" else mag
+        sum = (sum + mag) % 100
+        if sum == 0:
             zero_times += 1
 
-    sum = sum % 100
-    if sum == 0:
-        zero_times += 1
+    print(zero_times)
 
-print(zero_times)
+
+def part2():
+    zero_times: int = 0
+    sum: int = 50
+
+    for inp in inputs:
+        mag = int(inp[1:])
+        mag = -mag if inp[0] == "L" else mag
+
+        hundreds = abs(mag) // 100
+        if hundreds != 0:
+            zero_times += hundreds
+            if mag <= -100:
+                mag += hundreds * 100
+            elif mag >= 100:
+                mag -= hundreds * 100
+
+        old_sum = sum
+        sum = (sum + mag)
+        if (mag < 0):
+            if (old_sum > 0 and sum < 0):
+                zero_times += 1
+        elif (mag > 0):
+            if (old_sum < 100 and sum > 100):
+                zero_times += 1
+
+        sum = sum % 100
+        if sum == 0:
+            zero_times += 1
+
+    print(zero_times)
+
+
+part1()
+part2()
