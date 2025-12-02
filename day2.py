e38a9d978a5afacff3297f4e876e47bb2f9d7a4a
JidOsc <84289227+JidOsc@users.noreply.github.com>
2025-12-02 11:50:49 +0100

    ?

--- day2.py
+++ day2.py
@@ -2,7 +2,7 @@ inputs: list = open("inputs/day2.txt", "r").read().split(",")
 
 
 def part1():
-    def is_palindrome(id: str) -> bool:
+    def repeated_once(id: str) -> bool:
         if len(id) % 2 == 0:
             i = len(id) // 2
             if id[:i] == id[i:]:
@@ -19,31 +19,28 @@ def part1():
         id_range = end_id - start_id
 
         for i in range(id_range + 1):
-            if is_palindrome(str(start_id + i)):
+            if repeated_once(str(start_id + i)):
                 invalid_ids.append(start_id + i)
     print(sum(invalid_ids))
 
 
 def part2():
     def is_repeated(id: str) -> bool:
-        cur = ""
+        cur_pattern = ""
         for digit in id:
-            cur += digit
-
-            le = len(cur)
-            if le > len(id) // 2:
+            cur_pattern += digit
+            cur_length = len(cur_pattern)
+            if cur_length > len(id) // 2:
                 break
-
-            if len(id) % le == 0:
-                for cou in range(len(id) // le):
-                    if id[cou * le: (cou+1) * le] != cur:
+            if len(id) % cur_length == 0:
+                for cou in range(len(id) // cur_length):
+                    if id[cou * cur_length: (cou+1) * cur_length] != cur_pattern:
                         break
                 else:
                     return True
         return False
     
     invalid_ids: list = []
-    
     for id_pair in inputs:
         ids = id_pair.split("-")
 
