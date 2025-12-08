2086dc531bb5201ed4f129c89af7cfe5cab8022f
jidosc <oscar.jidemo@hotmail.se>
2025-12-08 14:07:58 +0100

    even worse

--- day8.py
+++ day8.py
@@ -10,34 +10,43 @@ Multiplicera N största circuits.
 """
 
 import math
+import time
 
 
-def distance_3d(pos_a, pos_b) -> float:
-    return math.sqrt(
-        (pos_b[0] - pos_a[0]) ** 2
-        + (pos_b[1] - pos_a[1]) ** 2
-        + (pos_b[2] - pos_a[2]) ** 2
-    )
+def closest_pair(junctions: set, circuits) -> tuple:
+    min_pair: set = set()
+    min_dist: float = 10000000
+
+    for x in junctions:
+        y = min(
+            filter(
+                lambda b: b != x and not shares_circuit({x, b}, circuits), junctions
+            ),
+            key=lambda b: math.dist(x, b),
+        )
+        dist = math.dist(x, y)
+        if dist >= min_dist:
+            continue
+        if not min_pair or dist < min_dist:
+            min_pair = {x, y}
+            min_dist = dist
+            continue
+    return min_pair
 
 
-def closest_pair(junctions, already_paired: list[set]) -> tuple:
-    return min(
-        *[
-            (x, y, distance_3d(x, y))
-            for x in junctions
-            for y in junctions
-            if x != y and any([not {x, y}.isdisjoint(s) for s in already_paired])
-        ],
-        key=lambda p: p[2],
-    )
+def shares_circuit(juncs: set, circuits) -> bool:
+    for circ in circuits:
+        if len(circ) <= 1:
+            continue
+        if juncs <= circ:
+            return True
+        elif not juncs.isdisjoint(circ):
+            return False
+    return False
 
 
-def shares_circuit(junc_a, junc_b, circuits) -> bool:
-    return any({junc_a, junc_b}.issubset(circ) for circ in circuits)
-
-
-def merge_circuits(junc_a, junc_b, circuits) -> list[list]:
-    merge_circs = list(filter(lambda c: junc_a in c or junc_b in c, circuits))
+def merge_circuits(juncs, circuits) -> list[list]:
+    merge_circs = list(filter(lambda c: not juncs.isdisjoint(c), circuits))
     new_circuit = merge_circs[0] | merge_circs[1]
 
     modified_circuits = circuits.copy()
@@ -48,13 +57,12 @@ def merge_circuits(junc_a, junc_b, circuits) -> list[list]:
 
 
 def part1():
-    TARGET_CONNECTIONS = 50
+    TARGET_CONNECTIONS = 1000
 
     junctions: set = {
         tuple(map(int, x.split(",")))
         for x in open("inputs/day8.txt", "r").read().splitlines()
     }
-    connections: list[set] = []
     connection_count = 0
     circuits: list[set] = []
 
@@ -62,17 +70,23 @@ def part1():
         circuits.append({j})
 
     while connection_count + 1 < TARGET_CONNECTIONS:
-        conn = closest_pair(junctions, connections)
-        connections.append({conn[0], conn[1]})
-        if not shares_circuit(conn[0], conn[1], circuits):
-            # print(f"Connects {conn}")
-            circuits = merge_circuits(conn[0], conn[1], circuits)
-            connection_count += 1
+        start = time.time()
+        conn = closest_pair(junctions, circuits)
+        start = time_part("CLOSEST", start)
+        # print(f"Connects {conn}")
+        circuits = merge_circuits(conn, circuits)
+        start = time_part("MERGE", start)
+        connection_count += 1
         # else:
         # print(f"Skips {conn}")
 
     circuits.sort(key=lambda c: len(c), reverse=True)
     print([len(c) for c in circuits])
+
+
+def time_part(part: str, start: float) -> float:
+    print(f"{part} took {time.time() - start}")
+    return time.time()
 
 
 part1()
