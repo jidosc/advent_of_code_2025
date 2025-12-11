4f201ca5a1106e9c3dce9039e69f41435dc0ba31
JidOsc <84289227+JidOsc@users.noreply.github.com>
2025-12-12 00:15:12 +0100

    Base case

--- day11.py
+++ day11.py
@@ -1,43 +1,39 @@
+from functools import cache
+
+data = open("inputs/day11.txt", "r").read().splitlines()
+inputs = [row.split(" ")[0].rstrip(":") for row in data]
+outputs = [row.split(" ")[1:] for row in data]
+register = {}
+for i in range(len(inputs)):
+    register[inputs[i]] = outputs[i]
+
+
 def part1():
-    data = open("inputs/day11.txt", "r").read().splitlines()
-
-    inputs = [row.split(" ")[0].rstrip(":") for row in data]
-    outputs = [row.split(" ")[1:] for row in data]
-    register = {}
-    for i in range(len(inputs)):
-        register[inputs[i]] = outputs[i]
-
-    print(valid_paths(register, "you", []))
+    pass
 
 
 def part2():
-    data = open("inputs/day11.txt", "r").read().splitlines()
-
-    inputs = [row.split(" ")[0].rstrip(":") for row in data]
-    outputs = [row.split(" ")[1:] for row in data]
-    register = {}
-    for i in range(len(inputs)):
-        register[inputs[i]] = outputs[i]
-
-    print(valid_paths(register, "svr", []))
+    print(valid_paths("svr", set()))
 
 
-def valid_paths(register, input, already_visited) -> int:
-    if is_dead_end(register, input, already_visited):
-        return 0
+dead_ends = []
+def valid_paths(input, already_visited: set) -> int:
     if input == "out":
-        return "dac" in already_visited and "fft" in already_visited
+        return 1 if {"dac", "fft"}.issubset(already_visited) else 0
+
+    if is_dead_end(input, already_visited):
+        return 0
     
-    travel_guide = already_visited[:]
-    travel_guide.append(input)
+    travel_guide = already_visited.copy()
+    travel_guide.add(input)
 
     total = 0
     for o in register[input]:
-        total += valid_paths(register, o, travel_guide)
+        total += valid_paths(o, travel_guide)
     return total
 
 
-def is_dead_end(register, input, already_visited) -> bool:
+def is_dead_end(input, already_visited: tuple) -> bool:
     if input in already_visited:
         return True
     return False
